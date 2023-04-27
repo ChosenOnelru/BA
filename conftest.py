1ed0c6dc2a6b6f2f9a2bd87d95ffa2b2a20e6b45
ChosenOnelru <chosenonex1@gmail.com>
2023-04-27 22:11:32 +0300

    added test find_NTD_by_number

--- conftest.py
+++ conftest.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 
-@pytest.fixture(scope='session') # задаем уровень использования фикстуры уровень функции/сессии/тд. по умолчанию - уровень функции
+@pytest.fixture(scope='session')
 def driver():
     print('start browser')
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
