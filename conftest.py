0fb117db75695c836a61e85c3f58212982d54514
ChosenOnelru <chosenonex1@gmail.com>
2023-04-28 11:09:14 +0300

    added test find_NTD_by_number

--- conftest.py
+++ conftest.py
@@ -2,10 +2,15 @@ import pytest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture(scope='session')
 def driver():
-    print('start browser')
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    print('\nstart browser')
+    options = Options()
+    options.accept_insecure_certs = True
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     driver.maximize_window()
     yield driver
+    print('\nquit browser...')
+    driver.quit()
